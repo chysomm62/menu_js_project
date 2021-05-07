42c57aa0bc751d398b231bf9b72a4eac42d5fc95
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-05-07 19:55:36 +0100

    refactored to add buttons dynamically

--- app.js
+++ app.js
@@ -71,6 +71,14 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "the  best dinner ever",
+    category: "dinner",
+    price: 44.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector('.section-center');
@@ -80,6 +88,11 @@ const filterBtn = document.querySelectorAll('.filter-btn');
 // load items
 window.addEventListener('DOMContentLoaded', () => {
   displayMenuItems(menu);
+
+  const categories = menu.map( (items) => {
+    return items.category;
+  });
+  console.log(categories);
 });
 
 
